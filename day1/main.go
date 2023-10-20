d5e9bb6c573ef131891282fd28199c059cd7a68c
Felipe <80273544+cabra-arretado@users.noreply.github.com>
2023-10-20 14:18:50 -0500

    day1 complete

--- day1/main.go
+++ day1/main.go
@@ -1,16 +1,44 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 )
 
-func read_file(file string) {
-	dt, err := os.ReadFile(file)
+func read_file(file_path string) {
+	file, err := os.Open(file_path)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Print(dt)
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+
+	total_cal := 0
+	max_cal := 0
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line != "" {
+			cal, err := strconv.Atoi(line)
+			if err != nil {
+				panic(err)
+			}
+			total_cal += cal
+			continue
+		}
+		if total_cal > max_cal {
+			fmt.Println("max_calories")
+			fmt.Println(max_cal)
+			fmt.Println("total_calories")
+			fmt.Println(total_cal)
+			max_cal = total_cal
+		}
+		total_cal = 0
+	}
+
 }
 
 func main() {
