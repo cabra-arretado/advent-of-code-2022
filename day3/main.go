ea1a8f49368fc1e48d717ef830296d601afdcff4
Felipe <80273544+cabra-arretado@users.noreply.github.com>
2023-10-21 19:20:00 -0500

    passing the first test

--- day3/main.go
+++ day3/main.go
@@ -31,28 +31,44 @@ func process_input(file_path string) int {
 	return total
 }
 
+func convert_rune(c rune) int {
+	// Lowercase item types a through z have priorities 1 through 26.
+	// Uppercase item types A through Z have priorities 27 through 52.
+	// Convert rune into the numbers given by the day3
+	ci := int(c)
+
+	if ci >= 97 && ci <= 122 {
+		return ci - 96
+	} else if ci >= 65 && ci <= 90 {
+		return ci - 64 + 26
+	}
+	panic("Did not have a match")
+}
+
 func process_line(l string) int {
 	half_index := len(l) / 2
 	arr_a := l[:half_index]
 	arr_b := l[half_index:]
 
-	for i := range arr_a {
-		if strings.ContainsRune(arr_b, rune(arr_a[i])) {
-			fmt.Println(arr_a[i])
+	var total int
+	for _, c := range arr_a {
+		if strings.ContainsRune(arr_b, c) {
+			total += convert_rune(c)
 		}
 	}
-
-	return 0
+	return total
 }
 
 func main() {
 	// read_file("./input.txt")
 
 	// Test cases
-	test_1 := process_line("vJrwpWtwJgWrhcsFMMfFFhFp")
-	// test_2 := process_line("jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL")
-	// test_3 := process_line("PmmdzqPrVvPwwTWBwg")
-	fmt.Println(test_1 == 16)
-	// fmt.Println(test_2 == 38)
-	// fmt.Println(test_3 == 42)
+	// test_1 := process_line("vJrwpWtwJgWrhcsFMMfFFhFp")
+	test_2 := process_line("jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL")
+	test_3 := process_line("PmmdzqPrVvPwwTWBwg")
+	// fmt.Println(test_1 == 16)
+	fmt.Println(test_2)
+	fmt.Println(test_2 == 38)
+	fmt.Println(test_3)
+	fmt.Println(test_3 == 42)
 }
